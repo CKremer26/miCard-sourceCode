d1182ee128f12060f567767552a668db029d3843
CKremer26 <99209633+CKremer26@users.noreply.github.com>
2023-01-23 14:08:32 -0500

    First Commit

---
+++ main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mi_card/page2.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomepage());
+  }
+}
+
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({super.key});
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/avatar.png'),
+              ),
+              Container(
+                  margin: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  child: Text(
+                    "Connor Kremer",
+                    style: GoogleFonts.changa(
+                        textStyle:
+                            const TextStyle(color: Colors.white, fontSize: 40)),
+                  )),
+              Container(
+                margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                child: Text(
+                  "Student",
+                  style: GoogleFonts.lato(
+                      textStyle:
+                          const TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                height: 60,
+                margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.phone_android),
+                    Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        child: const Text(
+                          "705-232-1932",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        )),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                height: 60,
+                margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.mail),
+                    Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        child: const Text(
+                          "example@smcdsb.on.ca",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        )),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const Page2())),
+        tooltip: "Next Page",
+        child: const Icon(Icons.arrow_forward_ios_sharp),
+      ),
+    );
+  }
+}
---
+++ page2.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
+
+class Page2 extends StatefulWidget {
+  const Page2({super.key});
+
+  @override
+  State<Page2> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  late double _currentSliderValue;
+  String? _sliderStatus;
+  FirebaseDatabase database = FirebaseDatabase.instance;
+
+  DatabaseReference dataRef = FirebaseDatabase.instance.ref('data');
+
+  late DatabaseReference counterRef;
+  late StreamSubscription counterStream;
+
+  void _reset() {
+    setState(() {
+      counterRef.set(0);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _currentSliderValue = 0;
+
+    counterRef = dataRef.child('counter');
+    counterStream = counterRef.onValue.listen((event) {
+      //print(event.snapshot.value);
+      setState(() {
+        _currentSliderValue = event.snapshot.value as double;
+      });
+      final data = event.snapshot.value;
+      if (data != null) {
+        setState(() {
+          _currentSliderValue = data as double;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: Colors.grey,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("$_currentSliderValue",
+                style: const TextStyle(color: Colors.black)),
+            Container(
+              width: 400,
+              child: CupertinoSlider(
+                key: const Key("slider"),
+                value: _currentSliderValue,
+                divisions: 10,
+                max: 100,
+                activeColor: CupertinoColors.systemGreen,
+                thumbColor: CupertinoColors.systemGreen,
+                onChangeStart: (double value) {
+                  setState(() {
+                    _sliderStatus = "Sliding";
+                  });
+                },
+                onChangeEnd: (double value) {
+                  setState(() {
+                    _sliderStatus = "Finished Sliding";
+                  });
+                },
+                onChanged: (double value) {
+                  setState(() {
+                    _currentSliderValue = value;
+                    counterRef.set(_currentSliderValue);
+                  });
+                },
+              ),
+            ),
+            Text(
+              _sliderStatus ?? "",
+              style: CupertinoTheme.of(context)
+                  .textTheme
+                  .textStyle
+                  .copyWith(fontSize: 12),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: ElevatedButton(
+                        onPressed: _reset, child: const Text("Reset")),
+                  ),
+                  ElevatedButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text("Go Back")),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
